63e778c3e464ad1bfff60680340d9f5d629706d3
Christian Hiricoiu <christian.hiricoiu@gmail.com>
2020-11-21 11:43:21 +0100

    update

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import sys
-import PyQt5.QtWidgets as Qt
+import requests
+import PyQt5.QtWidgets as QtWidgets
+import PyQt5.QtGui as QtGui
+import PyQt5.QtCore as QtCore
+
+from win32api import GetSystemMetrics
 
 
-class Example(Qt.QMainWindow):
+class MainWin(QtWidgets.QMainWindow):
     
     def __init__(self):
         super().__init__()
-        
+
+
+        self.img_label = QtWidgets.QLabel(self)        
+        self.img = QtGui.QImage()      
         self.initUI()
         
         
     def initUI(self):
         
-        self.setGeometry(300, 300, 800, 600)
+        self.setGeometry(0,0,GetSystemMetrics(0),GetSystemMetrics(1))
         self.setWindowTitle('Astronomy Picture of the Day')
-    
         self.show()
+    
+    def showImg(self, img_url):
+        self.img.loadFromData(requests.get(img_url).content)
+
+        self.pixmap = QtGui.QPixmap(self.img.scaledToHeight(1440))
+        self.img_label.setPixmap(self.pixmap)
+        self.img_label.resize(self.width(), self.height())
--- main.py
+++ main.py
@@ -1,27 +1,18 @@
 import requests
 import json
 import sys
+import datetime
+
 import PyQt5.QtWidgets as Qt
 
-from app import Example
-
-
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
-
-def request():
-    api_key = '4M5w5Q9nBUEvG66YPfj8H4dRJACj48nbdmGiEDV2'
-    url = 'https://api.nasa.gov/planetary/apod?'
-    query = ''
-
-    r1 = requests.get(url + 'api_key=' + api_key + query)
-    jprint(r1.json())
+from app import MainWin
+import apod_object_parser
 
 
 if __name__ == "__main__":
+    response = apod_object_parser.get_data('4M5w5Q9nBUEvG66YPfj8H4dRJACj48nbdmGiEDV2&date=2013-01-09')
+    url = apod_object_parser.get_hdurl(response)
     app = Qt.QApplication(sys.argv)
-    ex = Example()
+    ex = MainWin()
+    ex.showImg(url)
     sys.exit(app.exec_()) 
